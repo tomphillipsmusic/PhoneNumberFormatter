175e9e6f0c6c604d8869aa96dd76c6b3aca1225d
Tom Phillips <tomphillipsmusic@gmail.com>
2023-02-23 15:48:25 -0500

    Created Kata

--- PhoneNumberFormatter/main.swift
+++ PhoneNumberFormatter/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+    You will write a function that takes in an Int and formats it as an American mobile phone number. The function will return an optional String. If the given number can be formatted correctly, return the formatted String. If the number cannot be formatted correctly return nil
+ 
+    The American mobile phone number format typically consists of ten digits, written as three groups of numbers separated by hyphens. The format is:
 
+    (XXX) XXX-XXXX
+
+     The first three digits represent the area code, which corresponds to a geographic region in the United States. The next three digits represent the central office code, which identifies the specific phone exchange within the area code. The final four digits represent the line number, which is the unique number assigned to the phone line.
+
+     For example, a phone number with the area code 123, central office code 456, and line number 7890 would be written as:
+
+     (123) 456-7890
+ 
+    
+
+    Sample inputs / outputs
+ 
+    1234567890 -> "(123) 456-7890"
+    8675309 -> nil
+    12345678900 -> nil
+    123456789 -> nil
+    1 -> nil
+ 
+ */
+func format(phoneNumber: Int) -> String? {
+    // TODO: Write your implementation of the function
+    return nil
+}
+
+assert(format(phoneNumber: 1234567890) == Optional("(123) 456-7890)"))
+assert(format(phoneNumber: 8675309) == nil)
+assert(format(phoneNumber: 12345678900) == nil)
+assert(format(phoneNumber: 123456789) == nil)
+assert(format(phoneNumber: 1) == nil)
+assert(format(phoneNumber: 0987654321) == Optional("(098) 765-4321)"))
